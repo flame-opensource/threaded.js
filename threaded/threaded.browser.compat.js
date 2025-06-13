ffbda18d8f3eaa9d4cf0241c24d7604d0409c014
flame <flame.opensource.contact@gmail.com>
2025-06-13 16:25:42 +0100

    Add files via upload

--- threaded/threaded.browser.compat.js
+++ threaded/threaded.browser.compat.js
@@ -1,4 +1,4 @@
-import { Thread, ThreadExecutor, ThreadGroup, ThreadError, ThreadedTools } from './threaded/threaded.js';
+import { Thread, ThreadExecutor, ThreadGroup, ThreadError, ThreadedTools } from './threaded.js';
 
 window.Thread = Thread;
 window.ThreadExecutor = ThreadExecutor;
