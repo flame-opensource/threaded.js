f184df1c17397c31fc0f094a8a1344789521d291
flame <flame.opensource.contact@gmail.com>
2025-06-14 00:17:05 +0100

    Delete threaded/threaded.node.compat.js

--- threaded/threaded.node.compat.js
+++
@@ -1,9 +0,0 @@
-class ThreadedNodeCompat {
-    static supportNode(ThreadedTools) {
-        ThreadedTools.acorn = require('acorn');
-        ThreadedTools.walk = require('acorn-walk');
-        ThreadedTools.escodegen = require('escodegen');
-    }
-}
-
-module.exports = { ThreadedNodeCompat };
